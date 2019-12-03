986378e817c2a9274e1f28c2215d781fd00fed1a
Alexander Boris <intel777@users.noreply.github.com>
2019-12-03 18:30:21 +0200

    Sample startup config file

---
+++ sample-main.py
@@ -0,0 +1,14 @@
+from vkchatbot import Bot
+
+print('VkChatBot version 3.0.0 starting...')
+vkToken = 'YOUR_ACCESS_TOKEN_HERE'
+
+modules_to_load = ['loader', 'admin_tools', 'simple_chat', 'intel_ark', 'wallhaven', 'wit_ai', 'wikipedia', 'lurk',
+                   'yandex_tts', 'bugurt', 'rutracker', 'wolframalpha', 'rkn', 'everypixel', 'parliament', 'bssid_locator']
+
+print('[VKCB][Boot]Initializing Bot...')
+bot = Bot()
+bot.init(vkToken)
+for module in modules_to_load:
+    bot.load_module(module)
+bot.start_polling()
